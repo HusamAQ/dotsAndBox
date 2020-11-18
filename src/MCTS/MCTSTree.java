fbb52f93f73cd94957ce1041cbb1f50c25f8a694
HusamAQ <56165481+HusamAQ@users.noreply.github.com>
2020-11-18 14:23:59 +0100

    new

--- src/MCTS/MCTSTree.java
+++ src/MCTS/MCTSTree.java
@@ -5,8 +5,8 @@ public class MCTSTree {
     private MCTSNode root;
     private ArrayList<MCTSNode> treeNodes = new ArrayList<MCTSNode>();
 
-    public MCTSTree(int[][] matrix, int score){
-        root = new MCTSNode(matrix,score);
+    public MCTSTree(int[][] matrix, int score1, int score2, boolean botsTurn){
+        root = new MCTSNode(new State(matrix, score1, score2, botsTurn));
         treeNodes.add(root);
         generateChildren(root);
     }
@@ -14,9 +14,9 @@ public class MCTSTree {
     //TODO this method should generate all the children of this node.
     private void generateChildren(MCTSNode parent) {}
 
-    private MCTSNode inTree(int[][] matrix, int score){
+    private MCTSNode inTree(MCTSNode O){
         for(int i =0; i < treeNodes.size();i++){
-            if(treeNodes.get(i).equalsState(matrix,score)){
+            if(treeNodes.get(i).equals(O)){
                 return treeNodes.get(i);
             }
         }
