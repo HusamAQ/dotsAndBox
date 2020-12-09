9ed5296c5823e5689cdcde7fdf815b652cfc720d
jonathonbird <56203891+jonathonbird@users.noreply.github.com>
2020-12-09 17:35:08 +0100

    Revert "WARNING: WILL MOST LIKELY CAUSE ISSUES. Added the neural network and changed project to a Maven project"
    This reverts commit 61ea645791faf7ca8014a825d908b523de6944e2.

--- src/main/java/game/Test.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-import java.io.IOException;
-
-public class Test {
-	public static void main(String[] args) throws IOException, InterruptedException {
-		GameBoard g = new GameBoard(3,4);
-	}
-}
